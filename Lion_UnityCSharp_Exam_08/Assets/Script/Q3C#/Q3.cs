a65736b875f830bbd5b84d6e73af88294d9158ba
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-07-21 15:09:15 +0800

    FINISH_NEW

--- Lion_UnityCSharp_Exam_08/Assets/Script/Q3C#/Q3.cs
+++ Lion_UnityCSharp_Exam_08/Assets/Script/Q3C#/Q3.cs
@@ -15,6 +15,12 @@ public class Q3 : MonoBehaviour
             {
                 Vector3 pos = new Vector3(i  , j , 50);
                 Instantiate(Cube, pos, Quaternion.identity);
+                Vector3 pos2 = new Vector3(i, -j, 50);
+                Instantiate(Cube, pos2, Quaternion.identity);
+                Vector3 pos3 = new Vector3(-i+18  , j , 50);
+                Instantiate(Cube, pos3, Quaternion.identity);
+                Vector3 pos4 = new Vector3(-i+18, -j, 50);
+                Instantiate(Cube, pos4, Quaternion.identity);
             }
         }
     }
